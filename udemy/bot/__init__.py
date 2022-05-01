41efcb684bd0b988bf94d86e312a220486b09de9
BodyCatcher <100802051+BodyCatcher@users.noreply.github.com>
2022-05-01 23:59:23 +0300

    Update __init__.py

--- udemy/bot/__init__.py
+++ udemy/bot/__init__.py
@@ -1,4 +1,4 @@
-#   UdemyCourseScrapperBot - A Simple Udemy Free Courses Scrapper Bot (Updated)  
+# UdemyCourseScrapperBot - A Simple Udemy Free Courses Scrapper Bot (Updated)  
 
 # Copyright (C) 2022-Present by Body Catcher <https://github.com/BodyCatcher>
 
