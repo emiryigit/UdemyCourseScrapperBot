13716f8ac144054227cfebdf1d61038ad0f547e7
BodyCatcher <100802051+BodyCatcher@users.noreply.github.com>
2022-05-02 00:05:39 +0300

    Update __init__.py

--- udemy/duce/__init__.py
+++ udemy/duce/__init__.py
@@ -1,5 +1,5 @@
-# UdemyBot - A Simple Udemy Free Courses Scrapper
+# UdemyCourseScrapperBot - A Simple Udemy Free Courses Scrapper Bot (Updated)  
 
-# Copyright (C) 2021-Present Gautam Kumar <https://github.com/gautamajay52>
+# Copyright (C) 2022-Present by Body Catcher <https://github.com/BodyCatcher>
 
 from udemy.duce.duce import Scrapper
